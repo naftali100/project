ecaa33b57c5369e6eacbc2e77534b0a6fa875dce
naftali <n.besyakov@gmail.com>
2022-05-25 20:58:47 +0300

    update wall

--- include/Game/Wall.h
+++ include/Game/Wall.h
@@ -4,21 +4,17 @@
 
 #include "Entity.h"
 
+// basically a rectangle with collision tag set to wall
 class Wall : public Entity {
 public:
     Wall() {
         setCollisionTag(CollisionTag::wall);
     }
     virtual void update(const sf::Time& dt) override{};
-    virtual void handleEvent(const sf::Event& e){
-        // check in events map
-    };
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states = sf::RenderStates::Default) const override {
-        // debug draw
-        // white rectangle of global bound
-        sf::RectangleShape r{getSize()};
-        r.setPosition(getPosition());
-        target.draw(r);
+        // sf::RectangleShape r{getSize()};
+        // r.setPosition(getPosition());
+        // target.draw(r);
     }
 
 private:
