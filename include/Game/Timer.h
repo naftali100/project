391c0097c0fb2fc478056095d4901186587e88cc
naftali <n.besyakov@gmail.com>
2022-05-25 23:09:42 +0300

    update timer

--- include/Game/Timer.h
+++ include/Game/Timer.h
@@ -5,12 +5,14 @@
 #include <atomic>
 using Func = std::function<void()>;
 
-class MyTimer {
-    std::atomic<bool> active{ true };
-
+class MyTimer : public sf::Time {
 public:
+    void setTime(sf::Time);
+    void update(sf::Time);
     void setTimeout(Func function, int delay);
     void setInterval(Func function, int interval);
     void stop();
 
+private:
+    std::atomic<bool> active{ true };
 };
