37048749b4333e9ed90be58879deeba91cc62fed
naftali <n.besyakov@gmail.com>
2022-06-06 21:27:46 +0300

    update message bus

--- include/MessageBus.h
+++ include/MessageBus.h
@@ -2,55 +2,81 @@
 #define __MESSAGEBUS_H__
 #pragma once
 
-enum class MessageType {
+enum class MessageType
+{
     MessageId,
     BombJailed,
     BombTimedout,
     BombRemoveFromVector
 };
 
-class Message {
-
-};
-// MAYBE: this should be push/pop message and not post...
-// ANOTHER MAYBE: when message sent its propagate like event to all objects (add handleMessage to Entity)
-
 class MessageBus {
 public:
-    static void postMessage(MessageType m){
-        MessageBus::getInstance().m_messageTypeCounter[m]++;
-    };
-    template<typename T>
-    static void postMessage(MessageType m, T* attachment){
-        MessageBus::getMessageTypeWithAttachmentMap<T>().emplace(m ,attachment);
-    };
-    // static void postMessage(std::string){};
-    // static void postMessage(Message){};
-    
+    /// register
 
-    static int& getMessage(MessageType m){
-        // TODO: should this reset the messages count? or should we return read/write reference?
-        return MessageBus::getInstance().m_messageTypeCounter[m];
+    // just basic ping to all subscribers. needed?
+    static void addReceiver(std::function<void()> f) {
+        getInstance().m_subscribers.push_back(f);
+    };
+
+    static void addReceiver(MessageType type, std::function<void()> func) {
+        getInstance().m_subscribersWithType.push_back(std::pair{type, func});
+    };
+
+    template <typename T>
+    static void addReceiver(std::function<void(const T&)> func) {
+        m_subscribersWithArg<T>.push_back(func);
+    };
+
+    template <typename T>
+    static void addReceiver(MessageType type, std::function<void(const T&)> func) {
+        m_subscribersWithTypeAndArg<T>.push_back(std::pair{type, func});
+    };
+
+    /// send events
+
+    static void notify() {
+        for (auto& i : getInstance().m_subscribers) { i(); }
     }
-    template<typename AttachmentType>
-    static AttachmentType* getMessage(MessageType m){
-        return MessageBus::getMessageTypeWithAttachmentMap<AttachmentType>()[m];
+
+    static void notify(MessageType t) {
+        for (auto& i : getInstance().m_subscribersWithType) {
+            if (i.first == t)
+                i.second();
+        }
     }
-    // static int getMessage(std::string);
+
+    template <typename T>
+    static void notify(const T& t) {
+        for (auto i : m_subscribersWithArg<T>) {
+            i(t); 
+        }
+    }
+
+    template <typename T>
+    static void notify(MessageType type, const T& param) {
+        for (auto i : m_subscribersWithTypeAndArg<T>) {
+            if (i.first == type)
+                i.second(param);
+        }
+    }
 
 private:
-    template<typename T>
-    static std::map<MessageType, T*>& getMessageTypeWithAttachmentMap(){
-        static std::map<MessageType, T*> m_map;
-        return m_map;
-    }
-    std::map<MessageType, int> m_messageTypeCounter;
-    static MessageBus& getInstance(){
+    static MessageBus& getInstance() {
         static MessageBus instance;
         return instance;
     }
 
     MessageBus() = default;
     MessageBus(const MessageBus&) = delete;
+
+    std::vector<std::function<void()>> m_subscribers;
+    std::vector<std::pair<MessageType, std::function<void()>>> m_subscribersWithType;
+
+    template <typename T>
+    inline static std::vector<std::function<void(const T&)>> m_subscribersWithArg;
+
+    template <typename T>
+    inline static std::vector<std::pair<MessageType, std::function<void(const T&)>>> m_subscribersWithTypeAndArg;
 };
-#endif // __MESSAGEBUS_H__
+#endif  // __MESSAGEBUS_H__
