f7382fdf666adb8cc051ad6d9ddf3c8f4bcce2cb
naftali <n.besyakov@gmail.com>
2022-06-06 23:25:03 +0300

    update plog (smaller main :) )

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 
-#include <plog/Appenders/ColorConsoleAppender.h>
-#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Formatters/FuncMessageFormatter.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Initializers/ConsoleInitializer.h>
 #include <plog/Log.h>
 
 #include "Game.h"
 
 int main() {
     // init plog
-    plog::ColorConsoleAppender<plog::TxtFormatter> appiarer;
-    plog::init(plog::info, &appiarer);
+    plog::init<plog::FuncMessageFormatter>(plog::info, plog::streamStdOut);
 
     Game().run();
     return 0;
