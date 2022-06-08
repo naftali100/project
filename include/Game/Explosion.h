716a32217ccf570f9798acdb7dbaefdaa887870a
naftali <n.besyakov@gmail.com>
2022-06-08 14:17:30 +0300

    fix explosion for new duration animatable

--- include/Game/Explosion.h
+++ include/Game/Explosion.h
@@ -7,7 +7,7 @@
 class Explosion : public Animateable {
 public:
     Explosion(const sf::Vector2f position = sf::Vector2f{ 0.0, 0.0 })
-    :Animateable::Animateable(2,4,Textures::Explosion, 0.08, position)
+    :Animateable::Animateable(2,4,Textures::Explosion, 0.08, position, 0)
     {
         m_sprite.scale(.25 / 2.f,.25/ 2.f);
     }
